cd5a9d71c9a505db2642ad11b5af9aa346fae10b
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-09-15 13:17:32 -0700

    day 1

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,27 +2,39 @@
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+        smallest_index = i
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
         # Your code here
-
+        arr[smallest_index], arr[i] = arr[i], arr[smallest_index]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
-
+    swapped = True
+    while(swapped):
+        swapped = False
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
     return arr
+    
 
 '''
+Bubble Sort:
+We have to loop a certain number of times .. based on the swaps; don't know how many loops that will take 
+
+for loop within a while loop; checks if we switched anything... If we don't change anything.. its going to stop looping 
+Check 
+
 STRETCH: implement the Counting Sort function below
 
 Counting sort is a sorting algorithm that works on a set of data where
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,30 @@
 def linear_search(arr, target):
     # Your code here
-
-
-    return -1   # not found
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    
+        return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
-def binary_search(arr, target):
+#Expects a sorted array of integers
 
+def binary_search(arr, target):
+    #Assumption is a sorted array; lowest is the 0th index .. highest the last
+    low = 0
+    high = len(arr) - 1
     # Your code here
 
+    #Binary search looks for the middle of the array 
+    while low <= high: 
+        middle = (low + high) // 2
+        guess = arr[middle]
+        if guess == target: #You guessed correctly; return the middle
+            return middle
+        if guess > target: #You want to shorten the array behind the midpoint by decreasing the range
+            high = middle - 1 
+        else: #If the guess is less... you need to choose a later starting point;  increase low ... 
+            low = middle + 1
 
     return -1  # not found
